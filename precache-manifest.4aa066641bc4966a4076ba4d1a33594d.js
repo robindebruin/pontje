8a3908a0511c8273b25cbdbbe08400ded2d26657
robindebruin <robin_de_bruin@live.nl>
2020-02-18 09:49:06 +0100

    Updates

---
+++ precache-manifest.4aa066641bc4966a4076ba4d1a33594d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2971018c10bd071d35ef2d333a483ddb",
+    "url": "/pontje/index.html"
+  },
+  {
+    "revision": "cca8dac4787d847f70e1",
+    "url": "/pontje/static/css/main.d0ee8873.chunk.css"
+  },
+  {
+    "revision": "82bd0bf5e25537ea0d0b",
+    "url": "/pontje/static/js/2.45f18c30.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/pontje/static/js/2.45f18c30.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cca8dac4787d847f70e1",
+    "url": "/pontje/static/js/main.019b37e0.chunk.js"
+  },
+  {
+    "revision": "236b9af140cc7e1fb8a8",
+    "url": "/pontje/static/js/runtime-main.9773170d.js"
+  }
+]);
